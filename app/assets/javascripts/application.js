917a30add13f8160a61328fef0f297f7a2cdaec7
Onur Cem <onur.erdogan@ozguryazilim.com.tr>
2021-09-05 18:35:27 +0300

    Greeting added & zurb found. included & haml training

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require foundation
+$(document).on('turbolinks:load', function() {
+    $(function(){ $(document).foundation(); });
+});
+$(function(){ $(document).foundation(); });
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :time_records
-  root 'time_records#index'
+  root 'main#hello'
   get 'filter', to: 'time_records#filter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
