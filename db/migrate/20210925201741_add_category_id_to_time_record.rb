fa6d644ba991de5a3b08d3a1a718e69462ad26f5
onurcemer <onurcem.erdogan@outlook.com>
2021-09-28 01:35:06 +0300

    migrations deleted

--- db/migrate/20210925201741_add_category_id_to_time_record.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoryIdToTimeRecord < ActiveRecord::Migration[6.0]
-  def change
-    add_column :time_records, :category_id, :integer
-    add_index :time_records, :category_id
-  end
-end
